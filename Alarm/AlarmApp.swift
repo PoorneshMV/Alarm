2c06fd20651aedcb8c83dc81e54ce70d380eea1a
Ron <ronerez.math@gmail.com>
2022-11-01 22:17:38 +0200

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Ron Erez on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
